b34ec24d6a14cbb4bce13d2188c0898722447159
VeraKulemina <verakulemina@gmail.com>
2023-04-13 18:42:06 -0600

    Revert "initial commit"
    This reverts commit 33299518faaf18e1df224f8c84e11e2348c812e7.

--- App.js
+++ App.js
@@ -1,52 +1,15 @@
-import React, { useEffect, useState } from 'react';
-import { FlatList, Text, View, StyleSheet } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
-export default App = () => {
-  const [isLoading, setLoading] = useState(true);
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    const fetchData = () => {
-      fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1&sparkline=true&price_change_percentage=7d')
-        .then((response) => response.json())
-        .then((data) => setData(data))
-        .catch((error) => console.error(error))
-        .finally(() => setLoading(false));
-    };
-  
-    fetchData(); // call it once when component mounts
-  
-    const interval = setInterval(() => {
-      fetchData(); // call it every second
-    }, 2000);
-  
-    return () => clearInterval(interval); // cleanup function
-  }, []);
-  
-
+export default function App() {
   return (
-
     <View style={styles.container}>
-      {isLoading ? <Text>Loading...</Text> : 
-      ( <View style={{ flex: 1, flexDirection: 'column', justifyContent:  'space-between'}}>
-          <Text style={{ fontSize: 18, color: 'green', textAlign: 'center'}}>accets</Text>
-          <FlatList
-          data={data.data}
-          keyExtractor={(item) => item.id}
-          renderItem={({item}) => (
-          <Text style={styles.item} >
-          {item.symbol}, {item.priceUsd}
-          </Text>
-            )}
-          />
-        </View>
-      )}
+      <Text>Open hi </Text>
+      <StatusBar style="auto" />
     </View>
   );
-};
+}
 
-<<<<<<< HEAD
-=======
 const styles = StyleSheet.create({
   container: {
     flex: 1,
