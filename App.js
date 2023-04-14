511a99d811e8eea034e6fa67664f497d72ecad83
VeraKulemina <verakulemina@gmail.com>
2023-04-13 18:29:01 -0600

    broken app

--- App.js
+++ App.js
@@ -1,43 +1,47 @@
-import React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
-import { StatusBar } from 'expo-status-bar';
-import ListItem from './components/ListItem';
+import React, { useEffect, useState } from 'react';
+import { FlatList, Text, View, StyleSheet } from 'react-native';
 
 export default App = () => {
-    return  (
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = () => {
+      fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1&sparkline=true&price_change_percentage=7d')
+        .then((response) => response.json())
+        .then((data) => setData(data))
+        .catch((error) => console.error(error))
+        .finally(() => setLoading(false));
+    };
+  
+    fetchData(); // call it once when component mounts
+  
+    const interval = setInterval(() => {
+      fetchData(); // call it every second
+    }, 2000);
+  
+    return () => clearInterval(interval); // cleanup function
+  }, []);
+  
+
+  return (
+
     <View style={styles.container}>
-        <View style={styles.titleWrapper}>
-        <Text style={styles.largeTitle}>Hello World</Text>
-        {/* <StatusBar style="auto"/> */}
+      {isLoading ? <Text>Loading...</Text> : 
+      ( <View style={{ flex: 1, flexDirection: 'column', justifyContent:  'space-between'}}>
+          <Text style={{ fontSize: 18, color: 'green', textAlign: 'center'}}>accets</Text>
+          <FlatList
+          data={data.data}
+          keyExtractor={(item) => item.id}
+          renderItem={({item}) => (
+          <Text style={styles.item} >
+          {item.symbol}, {item.priceUsd}
+          </Text>
+            )}
+          />
         </View>
-        <View style={styles.divider}/>
-        <ListItem/>
+      )}
     </View>
-
-    );
-}
-
-const styles = StyleSheet.create({
-container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    },
-    titleWrapper:{
-    marginTop: 80,
-    paddingHorizontal: 16,
-    },
-    largeTitle: {
-    fornSize: 24,
-    fontWeight: "bold",
-    },
-    divider: {
-    height: StyleSheet.hairlineWidth,
-    backgroundColor: "#A9ABB1",
-    marginHorizontal: 16,
-    marginTop: 16,
-
-    }
-});
-
-
+  );
+};
 
--- components/ListItem.js
+++ components/ListItem.js
@@ -1,17 +1,23 @@
 import React from 'react';
-import { View, StyleSheet, Image } from 'react-native';
-import { TouchableOpacity } from 'react-native-gesture-handler';
+import { View, StyleSheet, Image, Text, TouchableOpacity } from 'react-native';
+// import { TouchableOpacity } from 'react-native-gesture-handler';
 
-export default ListItem=() =>{
+export default ListItem=({name, symbol, currentPrice, price7d, logo}) =>{
     return (
     <TouchableOpacity>
          <View style={styles.itemWrapper}>
             {/* left side */}
             <View style={styles.leftWrapper}>
-                <Image source={{ uri: "https://upload.wikimedia.org/wikipedia/commons/0/05/Ethereum_logo_2014.svg"}}/>
+                <Image source={{ uri: logo}} style={styles.image}/>
+                <View style={styles.titlesWrapper}></View>
+                <Text style={styles.title}>{name}</Text>
+                <Text style={styles.subtitle}>{symbol.toUpperCase()}</Text>
             </View>
             {/* right side */}
-            <View style={styles.rigthWrapper}></View>
+            <View style={styles.rigthWrapper}>
+                <Text style={styles.title}>{currentPrice}</Text>
+                <Text style={[styles.subtitle, {color: "red"}]}>{price7d}</Text>
+            </View>
         </View>
     </TouchableOpacity>
     )
@@ -19,5 +25,33 @@ export default ListItem=() =>{
 }
 
 const styles = StyleSheet.create({
-
-})
+    itemWrapper: {
+      paddingHorizontal: 16,
+      marginTop: 24,
+      flexDirection: "row",
+      justifyContent: "space-between",
+      alignItems: 'center',
+    },
+    leftWrapper: {
+      flexDirection: "row",
+      alignItems: 'center',
+    },
+    image: {
+      height: 48,
+      width: 48,
+    },
+    titlesWrapper: {
+      marginLeft: 8,
+    },
+    title: {
+      fontSize: 18,
+    },
+    subtitle: {
+      marginTop: 4,
+      fontSize: 14,
+      color: "#A9ABB1",
+    },
+    rightWrapper: {
+      alignItems: 'flex-end',
+    },
+  })
