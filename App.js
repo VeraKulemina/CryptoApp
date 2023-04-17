a27a6651db23dcbe55178432d19e44300e0a1601
VeraKulemina <verakulemina@gmail.com>
2023-04-17 06:59:45 -0600

    monday morning commit

--- App.js
+++ App.js
@@ -75,4 +75,5 @@ const styles = StyleSheet.create({
     marginTop: 16,
   }
 })
+
 // https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1&sparkline=true&price_change_percentage=7d
